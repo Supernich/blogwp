c612d05d6f5528c9de06d31d7b0f83301276514a
Supernich <supernich19@gmail.com>
2021-08-24 10:42:31 +0300

    Show pdf page as default, without adding .pdf in adress bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # TODO: Show pdf page by default
+  match 'posts/:id' => 'posts#show', :defaults => { format: 'pdf' }, via: [:get]
   root 'posts#index'
 
   resources :posts do
